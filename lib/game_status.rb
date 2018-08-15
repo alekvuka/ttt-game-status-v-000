a99fc78b7d8f3a0018db5142c298b803c2e81296
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-15 03:14:38 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -54,6 +54,17 @@ def over?(board)
   end
 end
 
+def winner(board)
+  winning_array = Array.new(3)
+  winning_array = won?(board)
+
+  if board[winning_array[0]] == "X"
+    return "X"
+  else
+    return "O"
+  end
+end 
+
 
 
 # Define your WIN_COMBINATIONS constant

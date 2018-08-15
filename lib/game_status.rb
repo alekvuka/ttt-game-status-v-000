9c9de0a1e8d35659949a4cf2c92cbd65ba0107bf
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-15 03:24:29 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -58,13 +58,13 @@ def winner(board)
   #winning_array = Array.new(3)
   winning_array = won?(board)
 
-  #if board[winning_array[0]] == "X"
-  #  return "X"
+  if board[winning_array[0]] == "X"
+    return "X"
   #elsif board[winning_array[0]] == "O"
   #  return "O"
 #  else
 #    return nil
-#  end
+  end
 
 end
 

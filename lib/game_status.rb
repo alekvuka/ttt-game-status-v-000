5a6c2fd8b9ab52e29f20e1ab8851226517b47103
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-15 00:20:46 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,8 +16,8 @@ WIN_COMBINATIONS.each do |win_combo|
 
    if position_1 == "X" && position_2 == "X" && position_3 == "X"
      return win_combo
-#   elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-#     return win_combo
+   elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+     return win_combo
    else
      return false
    end

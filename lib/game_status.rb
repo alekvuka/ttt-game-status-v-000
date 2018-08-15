2c551792c7142f503b2f5aa51643b0c50a3c3392
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-15 01:37:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,14 +23,14 @@ WIN_COMBINATIONS.each do |win_combo|
   false
 end
 
-#def full?(board)
-#  board.each do |i|
-#    if i == "" || i == " "
-#      return false
-#    end
-#  end
-#  return true
-#end
+def full?(board)
+  board.each do |i|
+    if i == "" || i == " "
+      return false
+    end
+  end
+  return true
+end
 
 
 
